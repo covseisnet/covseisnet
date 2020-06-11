a2bf893f5d7817767024a6f06381dc1f29f55a06
Cyril Journeau <cyril.journeau@univ-grenoble-alpes.fr>
2020-06-11 14:52:35 +0200

    Update plot_example_3.py

--- examples/plot_example_3.py
+++ examples/plot_example_3.py
@@ -8,9 +8,9 @@ Network covariance matrix spectral width: natural case
 
 This example shows how to calculate the spectral width of the network covariance matrix. It makes use of real seismic data acquired the 2010.10.14 by the Piton de la Fournaise seismic network. This day of data contains seismovolcanic signals linked to the pre-eruptive seismic swarm and to the beginning of the co-eruptive tremor starting at 15:20 (GMT). This basic example does not apply any synchronization or pre-processing.
 
-Considering a seismic network with 21 stations (using only the vertical component), the covariance is of dimensions 21 times 21. The following example use a Fourier estimation window of 50 seconds and is estimated over 40 consecutive windows. The overlapping between the Fourier windows is 50%, and the step between two consecutive averaging window is 50%.
+Considering 15 stations of the Piton de la Fournaise seismic network (using only the vertical component), the covariance is of dimensions 15 times 15. The following example use a Fourier estimation window of 20 seconds and is estimated over 15 consecutive windows. The overlapping between the Fourier windows is 50%, and the step between two consecutive averaging window is 50%.
 
-The lower the covariance matrix spectral width (dark blue), the more coherent the seismic wavefield. Note that the sampling rate of the seismic station is 20 Hz, the dark band at 10 Hz therefore corresponds to the Nyquist frequency (and should be disregarded).
+The lower the covariance matrix spectral width (dark blue), the more coherent the seismic wavefield. 
 """
 
 import covseisnet as csn
@@ -23,8 +23,8 @@ file_seismograms = '/path/to/data'
 stream = obspy.read(file_seismograms)
 
 # calculate covariance from stream
-window_duration_sec = 50
-average = 40
+window_duration_sec = 20
+average = 15
 times, frequencies, covariances = csn.covariancematrix.calculate(
     stream, window_duration_sec, average
 )
