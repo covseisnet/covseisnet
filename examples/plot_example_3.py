fc4c6db77877c4c9c94d5b1ca8f488b167467771
Cyril Journeau <cyril.journeau@univ-grenoble-alpes.fr>
2020-06-11 15:24:18 +0200

    Update plot_example_3.py

--- examples/plot_example_3.py
+++ examples/plot_example_3.py
@@ -6,7 +6,7 @@ Network covariance matrix spectral width: natural case
 =========================================================
 
 
-This example shows how to calculate the spectral width of the network covariance matrix. It makes use of real seismic data acquired the 2010.10.14 by the Piton de la Fournaise seismic network. This day of data contains seismovolcanic signals linked to the pre-eruptive seismic swarm and to the beginning of the co-eruptive tremor starting at 15:20 (GMT). This basic example does not apply any synchronization or pre-processing.
+This example shows how to calculate the spectral width of the network covariance matrix. It makes use of 6 hours of real seismic data acquired the 2010.10.14 (between 10:00 and 16:00 - GMT) by the Piton de la Fournaise seismic network. This dataset contains seismovolcanic signals linked to a pre-eruptive seismic swarm and to the beginning of a co-eruptive tremor starting at 15:20 (GMT). This basic example makes use of a stream with already synchronized traces and does not apply any pre-processing.
 
 Considering 15 stations of the Piton de la Fournaise seismic network (using only the vertical component), the covariance is of dimensions 15 times 15. The following example use a Fourier estimation window of 20 seconds and is estimated over 15 consecutive windows. The overlapping between the Fourier windows is 50%, and the step between two consecutive averaging window is 50%.
 
@@ -34,8 +34,8 @@ spectral_width = covariances.coherence(kind="spectral_width")
 
 # show network covariance matrix spectral width
 fig, ax = plt.subplots(1, constrained_layout=True)
-img = ax.pcolormesh(times, frequencies, spectral_width.T, rasterized=True)
+img = ax.pcolormesh(10+times/3600, frequencies, spectral_width.T, rasterized=True)
 ax.set_ylim([0, stream[0].stats.sampling_rate / 2])
-ax.set_xlabel("Times (seconds)")
+ax.set_xlabel("2010.10.14 (hours)")
 ax.set_ylabel("Frequency (Hz)")
 plt.colorbar(img).set_label("Covariance matrix spectral width")
