c12340e8c4769d7f01254ccf00e60336b43b52c6
Francis Tong <francistong@gmail.com>
2021-12-14 15:03:53 +0100

    add __version__ attribute to package

--- covseisnet/__init__.py
+++ covseisnet/__init__.py
@@ -16,3 +16,6 @@ from .correlationmatrix import CorrelationMatrix
 from .traveltime import TravelTime
 from .beam import Beam
 from .arraystream import read
+from pkg_resources import get_distribution
+
+__version__ = get_distribution('covseisnet').version
